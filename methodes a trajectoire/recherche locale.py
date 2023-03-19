10e79dcec54caa28f23fa0b37fd02ca67eda6015
HOUD-FatimaEzzahra <fatimaezzahrahoud14@gmail.com>
2023-03-19 12:14:27 +0000

    recherche locale

---
+++ methodes a trajectoire/recherche locale.py
@@ -0,0 +1,47 @@
+import random
+
+# La fonction recherche_locale prend en entrée une solution initiale x_init et un nombre maximum
+# d itérations max_iterations. La fonction effectue une boucle de recherche locale en générant
+# une nouvelle solution voisine à chaque itération et en acceptant la nouvelle solution si elle
+# améliore le coût de la solution courante. La fonction renvoie la solution optimale trouvée.
+def recherche_locale(x_init, max_iterations):
+    # Initialisation
+    x = x_init
+    cout_x = f(x)
+    iteration = 0
+
+    # Boucle de recherche locale
+    while iteration < max_iterations:
+        # Génération d'une nouvelle solution voisine
+        x_prime = voisin(x)
+        cout_x_prime = f(x_prime)
+
+        # Acceptation de la nouvelle solution si elle est meilleure
+        if cout_x_prime < cout_x:
+            x = x_prime
+            cout_x = cout_x_prime
+
+        iteration += 1
+
+    return x
+
+
+
+#La fonction f(x) définit la fonction objectif f(x) = x^2 - 6x + 8 que nous cherchons à minimiser.
+def f(x):
+    return x ** 2 - 6 * x + 8
+
+
+#La fonction voisin(x) génère une nouvelle solution voisine en ajoutant ou soustrayant un nombre
+# aléatoire à la solution courante.
+def voisin(x):
+    return x + random.uniform(-1, 1)
+
+
+# Exemple d'utilisation de la fonction de recherche locale
+x_init = 5.0
+max_iterations = 1000
+solution_optimale = recherche_locale(x_init, max_iterations)
+
+print("Solution optimale trouvée: ", solution_optimale)
+print("Cout de la solution optimale: ", f(solution_optimale))

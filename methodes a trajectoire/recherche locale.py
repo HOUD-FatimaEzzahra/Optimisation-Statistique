e263d0d14df0aa37ff79314640d12fb8f287d20d
HOUD-FatimaEzzahra <fatimaezzahrahoud14@gmail.com>
2023-04-06 15:24:18 +0000

    recherche locale

--- methodes a trajectoire/recherche locale.py
+++ methodes a trajectoire/recherche locale.py
@@ -1,43 +1,38 @@
 import random
 
-def recherche_locale(x_init, max_iterations):
-    # Initialisation
-    x = x_init
-    cout_x = f(x)
-    iteration = 0
 
-    # Boucle de recherche locale
-    while iteration < max_iterations:
-        # Génération d'une nouvelle solution voisine
-        x_prime = voisin(x)
-        cout_x_prime = f(x_prime)
+def recherche_locale(f, E):
+    # Générer une solution initiale x0 dans E
+    x0 = random.choice(E)
+    x_best = x0
 
-        # Acceptation de la nouvelle solution si elle est meilleure
-        if cout_x_prime < cout_x:
-            x = x_prime
-            cout_x = cout_x_prime
+    i = 1
+    while True:
+        # Générer une solution dans le voisinage de x
+        x = random.choice(N(x_best))
 
-        iteration += 1
+        # Mettre à jour la solution
+        if f(x) < f(x_best):
+            x_best = x
+            i += 1
+        else:
+            # STOP si le critère d'arrêt est satisfait
+            break
 
-    return x
+    return x_best
 
 
-
-#La fonction f(x) définit la fonction objectif f(x) = x^2 - 6x + 8 que nous cherchons à minimiser.
+# Exemple d'utilisation
 def f(x):
-    return x ** 2 - 6 * x + 8
+    # Fonction à minimiser
+    return x ** 2
 
 
-#La fonction voisin(x) génère une nouvelle solution voisine en ajoutant ou soustrayant un nombre
-# aléatoire à la solution courante.
-def voisin(x):
-    return x + random.uniform(-1, 1)
+def N(x):
+    # Voisinage de x : x-1, x+1
+    return [x - 1, x + 1]
 
 
-# Exemple d'utilisation de la fonction de recherche locale
-x_init = 5.0
-max_iterations = 1000
-solution_optimale = recherche_locale(x_init, max_iterations)
-
-print("Solution optimale trouvée: ", solution_optimale)
-print("Cout de la solution optimale: ", f(solution_optimale))
+E = [-5, -4, -3, -2, -1, 0, 1, 2, 3, 4, 5]
+x_min = recherche_locale(f, E)
+print("Minimum trouvé :", x_min, "f(", x_min, ") =", f(x_min))
